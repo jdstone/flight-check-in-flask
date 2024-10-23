f6095a6cec451a032f81e52014175d6514a1be75
J.D. Stone <jdstone1@gmail.com>
2024-10-23 12:52:58 -0700

    code cleanup, add error logging

--- app/checkin.py
+++ app/checkin.py
@@ -5,31 +5,7 @@ from flask import Blueprint, request, current_app
 import logging
 from app.southwest import checkin_review
 
-
 bp = Blueprint('checkin', __name__, url_prefix='/')
-
-def my_job():
-    print('this is my job')
-
-
-# if not scheduler.get_job('my_job1'):
-#     scheduler.add_job(id='my_job1', func=my_job, trigger="date", run_date=datetime(2024, 11, 29, 15, 50, 0))
-
-# scheduler.add_job(id='my_job1', func=my_job, trigger="date", run_date=datetime(2024, 11, 14, 22, 39, 0))
-# print(f"My JOB: {scheduler.get_job('my_job1')}")
-
-# scheduler.scheduler.remove_jobstore('default')
-# scheduler.scheduler.remove_jobstore('SQlite')
-# scheduler.scheduler.remove_all_jobs()
-
-# scheduler.remove_jobstore('default')
-# scheduler.remove_jobstore('SQlite')
-# scheduler.remove_all_jobs()
-
-# while True:
-#     scheduler.scheduler.print_jobs()
-#     time.sleep(15)
-
 
 
 @bp.post("/schedule-checkin")
@@ -41,16 +17,16 @@ def get_passenger_data():
         conf_number = data['conf_number']
         first_name = data['first_name']
         last_name = data['last_name']
+        current_app.logger.debug(f"THIS DATA: {data}")
         # flight_date sent in format: MM/DD/YY
         # flight_time sent in format: HH:MM
         return create_job(flight_date, flight_time, conf_number, first_name, last_name)
 
-    return {"error": "Request must be JSON"}, 415
+    return {"get_passenger.error": "Request must be JSON"}, 415
 
 def create_job(flight_date, flight_time, conf_number, first_name, last_name):
     job_id = conf_number
     run_time = calculate_checkin_time(flight_date, flight_time)
-    print(run_time)
 
     if not scheduler.get_job(job_id):
         scheduler.add_job(
@@ -60,7 +36,11 @@ def create_job(flight_date, flight_time, conf_number, first_name, last_name):
             trigger="date",
             run_date=run_time
         )
-        return {"status": "Success"}
+        current_app.logger.info(f"Check-in scheduled for {conf_number}, {flight_date} at {flight_time}")
+
+        return {"scheduler.status": "Success"}
+
+    current_app.logger.error(f"Job already exists for job ID: {job_id}")
 
     return {"error": f"{job_id} already exists"}
 
