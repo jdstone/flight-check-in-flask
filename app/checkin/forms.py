92489ea63a986df76e69935f46f1e5e64826f9e6
J.D. Stone <jdstone1@gmail.com>
2024-10-26 16:12:14 -0700

    form improvement

--- app/checkin/forms.py
+++ app/checkin/forms.py
@@ -6,13 +6,8 @@ from wtforms.validators import ValidationError, Length, DataRequired
 class CheckinForm(FlaskForm):
     first_name     = StringField('First Name', [Length(min=1, max=25), DataRequired()])
     last_name      = StringField('Last Name', [Length(min=1, max=35), DataRequired()])
-    conf_num       = StringField('Confirmation #', [Length(min=6, max=6), DataRequired()])
+    conf_num       = StringField('Confirmation Number', [Length(min=6, max=6), DataRequired()])
     flight_time    = TimeField('Flight Departure Time', [DataRequired()], format='%H:%M')
     flight_date    = DateField('Flight Departure Date', [DataRequired()], format='%Y-%m-%d')
     submit         = SubmitField('Check in to flight')
-
-    def validate_conf_num(self, conf_num):
-        if int(len(self.conf_num.data)) != 6:
-            flash('Confirmation number is not long enough')
-            raise ValidationError('Confirmation number is not long enough')
 
