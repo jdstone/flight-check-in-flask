a136a1dc3d41233f2fc15afe9a9dd1d660aeb183
JD Stone <jdstone1@gmail.com>
2024-10-26 14:41:50 -0700

    Check in template & forms (#3)

--- app/__init__.py
+++ app/__init__.py
@@ -35,6 +35,9 @@ def create_app(config_class=os.getenv('FLASK_ENV') or 'default'):
     from . import schedule
     app.register_blueprint(schedule.bp)
 
+    from .checkin import bp as checkin_bp
+    app.register_blueprint(checkin_bp, url_prefix='/')
+
     ###################################################
     #### Error Logging to File - For Production
     ###################################################
