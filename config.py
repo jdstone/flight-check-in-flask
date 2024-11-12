f3fe25ae7607930364a0709b26248ec17a9bde47
JD Stone <jdstone1@gmail.com>
2024-11-12 09:42:19 -0800

    change default config and other values

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     TESTING = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
     SCHEDULER_API_ENABLED = True
-    SCHEDULER_JOBSTORES = {'default': SQLAlchemyJobStore(url='sqlite:///job_scheduler.db')}
+    SCHEDULER_JOBSTORES = {'default': SQLAlchemyJobStore(url=f"sqlite:///{os.getenv('SCHEDULER_JOBSTORE_FILE', 'job_scheduler.sqlite')}")}
 
 
 class TestConfig(Config):
@@ -21,21 +21,20 @@ class TestConfig(Config):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SW_REVIEW_API_URL = os.environ.get('SW_RV_API_URL')
-    SW_CONFIRM_API_URL = os.environ.get('SW_CM_API_URL')
+    SW_REVIEW_API_URL = os.getenv('SW_RV_API_URL')
+    SW_CONFIRM_API_URL = os.getenv('SW_CM_API_URL')
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SW_REVIEW_API_URL = "https://www.southwest.com/api/air-checkin/v1/air-checkin/page/air/check-in/review"
-    SW_CONFIRM_API_URL = "https://www.southwest.com/api/air-checkin/v1/air-checkin/page/air/check-in/confirmation"
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SW_REVIEW_API_URL = os.getenv('SW_RV_API_URL', 'https://www.southwest.com/api/air-checkin/v1/air-checkin/page/air/check-in/review')
+    SW_CONFIRM_API_URL = os.getenv('SW_CM_API_URL', 'https://www.southwest.com/api/air-checkin/v1/air-checkin/page/air/check-in/confirmation')
 
 
 config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
     'testing': TestConfig,
-    'default': DevelopmentConfig,
+    'default': ProductionConfig,
 }
-
